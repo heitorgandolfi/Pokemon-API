e3dc55fba83baa8adc7a7ee6506cf759fd525adb
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2023-01-02 14:19:51 -0300

    update JS
    Testando funcionalidades iniciais da aplicação e retorno de dados da API.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+const form = document.querySelector(".form");
+const inputSearch = document.querySelector(".inputSearch");
+const nome = document.getElementById("nome");
+const img = document.getElementById("img")
+const botao = document.getElementById("botao");
+
+
 const getPokemon = async (pokemon) => {
     const apiURL = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
@@ -7,7 +14,15 @@ const getPokemon = async (pokemon) => {
 
 const showPokemon = async (pokemon) => {
     const data = await getPokemon(pokemon);
+
+    nome.innerHTML = data.name;
+    img.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`
+    inputSearch.value = "";
+
     console.log(data);
 }
 
-showPokemon("pikachu");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    showPokemon(inputSearch.value)
+})
